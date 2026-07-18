ee4fe675e971965e23b5d518c57d85f9a7be3811
NITINCHANDER007 <157805011+NITINCHANDER007@users.noreply.github.com>
2026-07-18 19:37:58 +0530

    Update main.tsx

--- artifacts/nisha-godani/src/main.tsx
+++ artifacts/nisha-godani/src/main.tsx
@@ -1,7 +1,12 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 
-import App from './App';
+import App from './App'
+import './index.css'
 
-import './index.css';
-
-createRoot(document.getElementById('root')!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <>
+    <App />
+    <Analytics />
+  </>,
+)
